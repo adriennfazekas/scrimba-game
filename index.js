d9a53b7a5f0478d12c64504b38de0176b2f70e93
Fazekas Adrienn <adreeja17@gmail.com>
2023-03-16 17:01:44 +0100

    refactoring

--- index.js
+++ index.js
@@ -4,30 +4,7 @@ import Character from "./character.js"
 let monsterArray = ["orc", "demon", "goblin"]
 const attackBtn = document.getElementById("attack-button")
 
-function getNewMonster() {
-    const nextMonsterData = characters[monsterArray.shift()]
-    return nextMonsterData ? new Character(nextMonsterData) : {}
-}
-
-function endGame() {
-    const endMessage = hero.health === 0 && monster.health === 0 ? "No victors - all creatures are dead"
-        : hero.health <= 0 ? "The monsters are Victorious"
-        : "The Wizard Wins"
-    const endEmoji = hero.health > 0 ? "🔮" : "☠️"
-    setTimeout(()=> {
-        document.getElementById("main-section").innerHTML = 
-            `<div class="end-game">
-                <h2>Game Over</h2>
-                <h3>${endMessage}</h3>
-                <p class="end-emoji">${endEmoji}</p>
-            </div>`
-            attackBtn.remove()
-    }, 1500)
-     
-}
-
 function attack() { 
-
     hero.getDiceHtml()
     monster.getDiceHtml()
     hero.takeDamage(monster.currentDiceScore)
@@ -63,3 +40,24 @@ function render() {
 const hero = new Character(characters.hero)
 let monster = getNewMonster()
 render()
+
+function getNewMonster() {
+    const nextMonsterData = characters[monsterArray.shift()]
+    return nextMonsterData ? new Character(nextMonsterData) : {}
+}
+
+function endGame() {
+    const endMessage = hero.health === 0 && monster.health === 0 ? "No victors - all creatures are dead"
+        : hero.health <= 0 ? "The monsters are Victorious"
+        : "The Wizard Wins"
+    const endEmoji = hero.health > 0 ? "🔮" : "☠️"
+    setTimeout(()=> {
+        document.getElementById("main-section").innerHTML = 
+            `<div class="end-game">
+                <h2>Game Over</h2>
+                <h3>${endMessage}</h3>
+                <p class="end-emoji">${endEmoji}</p>
+            </div>`
+            attackBtn.remove()
+    }, 1500)     
+}
--- utils.js
+++ utils.js
@@ -1,13 +1,13 @@
 
 export function getDiceRollArray(diceCount) {
-    const randomNumberArray = new Array(diceCount).fill(0).map(function() {
+    const randomNumberArray = new Array(diceCount).fill(0).map( () => {
         return Math.floor(Math.random() * 6) +1
     })
     return randomNumberArray 
 }
 
 export function getDicePlaceholder(diceCount) {
-    const placeholderArray = new Array(diceCount).fill().map(function() {
+    const placeholderArray = new Array(diceCount).fill().map( () => {
         return `<div class="placeholder-dice"></div>`
     }).join('')
     return placeholderArray
